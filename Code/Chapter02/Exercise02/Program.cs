68b7fdbf2fa26ce564e65c53003517c7e4f0f2d1
belousovfil <belousov.fil@mail.ru>
2024-05-18 18:22:21 +0300

    Added two exercies

---
+++ Code/Chapter02/Exercise02/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*
+ Console.WriteLine("----------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("{0, -20} {1, 5} {2, 20} {3, 30}","Type","Bytes of memory","min","max");
+Console.WriteLine("----------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(SByte),SByte.MinValue,SByte.MaxValue);
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(Byte),Byte.MinValue,Byte.MaxValue); 
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(short),short.MinValue,short.MaxValue); 
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(ushort),ushort.MinValue,ushort.MaxValue);
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(int),int.MinValue,int.MaxValue); 
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(uint),uint.MinValue,uint.MaxValue); 
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(long),long.MinValue,long.MaxValue);
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(ulong),ulong.MinValue,ulong.MaxValue);
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(float),float.MinValue,float.MaxValue);
+Console.WriteLine("{0, -20} {1, 5} {2, 30} {3, 30}","Sbite",sizeof(double),double.MinValue,double.MaxValue);  
+Console.WriteLine("----------------------------------------------------------------------------------------------------------------"); */
+
+
+
+/*string phrase = "Giraf Academy";
+char grade = 'A';
+int age = 30;
+double gpa = 3.3; // decimal  for money 0,29 cents.
+bool isMale = true;
+Console.WriteLine(phrase);
+Console.WriteLine("Giraf \n move on next line down");  
+Console.WriteLine(phrase.Length);
+  */
+
+
+
+/* //⌨️ (2:58:45) Building an Exponent Method
+Console.WriteLine(GetPow_METHOD(  3, 2, "5"));
+
+static int GetPow_METHOD(int baseNum_VARIABLE, int powNum_VARIABLE, string Str_VARIABLE)
+{
+    //baseNum*powNum
+    char grade = 'A';
+    int result_VARIABLE ;
+    string phrase = "Giraf Academy";
+    //for (int i=0; i<powNum_VARIABLE; i++)
+    {
+//result_VARIABLE=result_VARIABLE*baseNum_VARIABLE;
+result_VARIABLE=powNum_VARIABLE*baseNum_VARIABLE;
+    }
+    return (powNum_VARIABLE);//   phrase;//   result_VARIABLE;//   grade;//
+}*/
+
+//⌨️ (3:24:34) Classes & Objects
